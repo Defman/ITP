0e939897e4f68b142b0353438560c25cd342fc71
The_Defman <jacob@rosborg.dk>
2018-08-25 21:37:40 +0200

    uge1

---
+++ 1uge/DieCup/Die.java
@@ -0,0 +1,36 @@
+// import class for generation of random numbers
+import java.util.Random;
+
+/**
+ * This class models a Die (terning)
+ * 
+ * @author Kurt Jensen
+ * @version 2017-01-05
+ */
+public class Die {
+    private Random random;   //used for generation of random numbers
+    private int eyes;       //number of eyes shown
+
+    /**
+     * Constructor for Die objects
+     */
+    public Die() {
+        random = new Random();
+        this.roll();
+    }
+
+    /**
+     * Obtain a new number of eyes for this die
+     */
+    public int roll() {
+        this.eyes = random.nextInt(6) + 1;
+        return getEyes();
+    }
+
+    /**
+     * Return the number of eyes shown by this die
+     */
+    public int getEyes() {
+        return this.eyes;
+    }
+}
---
+++ 1uge/DieCup/DieCup.java
@@ -0,0 +1,61 @@
+/**
+ * This class models a DieCup (raflebæger)
+ * 
+ * @author Kurt Jensen
+ * @version 2017-05-01
+ **/
+public class DieCup {
+    private Die d1;   //first die
+    private Die d2;   //second die
+    private int maxEyes;
+    
+    /**
+     * Constructor for DieCup objects
+     */
+    public DieCup() {
+        this(new Die(), new Die());
+    }
+    
+    /**
+     * Constructor for DieCup objects
+     */
+    public DieCup(Die d1, Die d2) {
+        this.d1 = d1;
+        this.d2 = d2;
+        this.maxEyes = 0;
+    }
+    
+    public void multipleRolls(int noOfRolls) {
+        DieCup dieCup = new DieCup();
+        double average = 0;
+        for (int i = 0; i < noOfRolls; i++) {
+            System.out.printf("Throw no %d: %d \n", i + 1, dieCup.roll());
+            average = (average * i + dieCup.getEyes()) / (i + 1);
+        }
+        System.out.printf("Average no of eyes: %.2f\n", average);
+    }
+    
+     /**
+     * Obtain a new number of eyes for both dies
+     */
+    public int roll() {
+        int sum = this.d1.roll() + this.d2.roll();
+        this.maxEyes = sum > maxEyes ? sum : maxEyes;
+        return sum;
+    }
+    
+    /**
+     * Return the sum of the number of eyes shown by the two dies
+     */
+    public int getEyes() {
+        return this.d1.getEyes() + this.d2.getEyes();
+    }
+    
+    public int getMaxEyes() {
+        return this.maxEyes;
+    }
+    
+    public void resetMaxEyes() {
+        this.maxEyes = 0;
+    }
+}
